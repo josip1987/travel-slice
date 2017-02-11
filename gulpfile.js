33d199aeba1c4e4bbfb2b2f34781b2863f295fbb
Josip <josip.susic@hotmail.com>
2017-02-11 21:03:28 +0100

    added build task to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,4 @@ require('./gulp/tasks/watch');
 require('./gulp/tasks/sprites');
 require('./gulp/tasks/scripts'); //intergrate webpack
 require('./gulp/tasks/modernizr');
+require('./gulp/tasks/build');
